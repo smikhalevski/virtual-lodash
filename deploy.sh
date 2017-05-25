1960589a614dd56bc1ba78c35ca8c79032fe0080
Savva Mikhalevski <smikhalevski@gmail.com>
2017-05-25 03:51:58 +0300

    Moved deploy scripts to separate file

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex;
+
+VERSION=$(npm v lodash version);
+if grep -q "$VERSION" package.json;
+ then VERSION=$VERSION-$TRAVIS_BUILD_NUMBER;
+fi;
+npm version "$VERSION" -m 'Release v%s';
+
+cp README.md LICENSE package.json build;
+cd build;
+
+npm publish;
+git push origin HEAD:$TRAVIS_BRANCH;
