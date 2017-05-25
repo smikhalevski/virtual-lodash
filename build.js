4842725510a6a5cbdddc267a31f34428fb07d50f
Savva Mikhalevski <smikhalevski@gmail.com>
2017-05-25 23:56:06 +0300

    Added tests; Added varargs support

--- build.js
+++ build.js
@@ -1,38 +1,64 @@
-import fs from 'fs';
-import _, {isFunction, range} from 'lodash';
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+const getParameterNames = require('get-parameter-names');
 
-function getArgs(func) {
-  // First match everything inside the function argument parens.
-  var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+const BUILD_DIR = './build';
 
-  // Split the arguments string into an array comma delimited.
-  return args.split(',').map(function(arg) {
-    // Ensure no inline comments are parsed and trim the whitespace.
-    return arg.replace(/\/\*.*\*\//, '').trim();
-  }).filter(function(arg) {
-    // Ensure no undefined values are added.
-    return arg;
-  });
+function createModuleSource(key, value) {
+  // Prevent baseSetToString to mimic original toString.
+  value.toString = Function.prototype.toString;
+
+  if (value.toString().includes('arguments') || _.isNative(value)) {
+    return `var _${key} = require('lodash/${key}');
+
+module.exports = function ${key}() {
+  var length = arguments.length;
+  var args = new Array(length + 1);
+  args[0] = this;
+  for (var i = 0; i < length; ++i) {
+    args[i + 1] = arguments[i];
+  }
+  return _${key}.apply(void 0, args);
+};`;
+  }
+
+  const params = getParameterNames(value);
+  if (params.length) {
+    params[0] = 'this';
+
+    return `var _${key} = require('lodash/${key}');
+
+module.exports = function ${key}(${params.slice(1)}) {
+  return _${key}(${params});
+};`;
+  }
+
+  return `module.exports = require('lodash/${key}');`;
 }
 
-let index = '';
+function createIndexSource(wrappedKeys) {
+  return `module.exports = {
+  ${wrappedKeys.map(key => `${key}: require('./${key}')`).join(',\n  ')}
+};`;
+}
 
-fs.mkdirSync('./build');
+fs.mkdirSync(BUILD_DIR);
 
-for (const [key, value] of Object.entries(_)) {
-  if (isFunction(value)) {
+const wrappedKeys = [];
 
-    if (fs.existsSync(`node_modules/lodash/${key}.js`)) {
-      const args = getArgs(value);
-      args[0] = 'this';
+for (const key of Object.keys(_)) {
+  if (_.isFunction(_[key]) && fs.existsSync(`node_modules/lodash/${key}.js`)) {
+    wrappedKeys.push(key);
 
-      fs.writeFileSync(`./build/${key}.js`, `
-var _${key} = require('lodash/${key}');
-
-module.exports = function ${key}(${args.slice(1)}) {
-  return _${key}.call(${args});
-};
-    `);
-    }
+    fs.writeFileSync(
+        path.join(BUILD_DIR, key + '.js'),
+        createModuleSource(key, _[key]) + '\n'
+    );
   }
 }
+
+fs.writeFileSync(
+    path.join(BUILD_DIR, 'index.js'),
+    createIndexSource(wrappedKeys) + '\n'
+);
---
+++ test/build.spec.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const assert = require('assert');
+
+describe('build.js', () => {
+
+  it('creates index file', () => {
+    const _ = require('../build/index');
+    assert('cloneDeepWith' in _, 'Method cloneDeepWith not found in index.js exports');
+  });
+
+  it('clones arguments for functions with varargs', () => {
+    const invokeMapSource = fs.readFileSync('./build/invokeMap.js');
+
+    assert.equal(invokeMapSource, `var _invokeMap = require('lodash/invokeMap');
+
+module.exports = function invokeMap() {
+  var length = arguments.length;
+  var args = new Array(length + 1);
+  args[0] = this;
+  for (var i = 0; i < length; ++i) {
+    args[i + 1] = arguments[i];
+  }
+  return _invokeMap.apply(void 0, args);
+};
+`);
+  });
+
+  it('clones arguments for native functions', () => {
+    const isArraySource = fs.readFileSync('./build/isArray.js');
+
+    assert.equal(isArraySource, `var _isArray = require('lodash/isArray');
+
+module.exports = function isArray() {
+  var length = arguments.length;
+  var args = new Array(length + 1);
+  args[0] = this;
+  for (var i = 0; i < length; ++i) {
+    args[i + 1] = arguments[i];
+  }
+  return _isArray.apply(void 0, args);
+};
+`);
+  });
+
+  it('creates simple for functions with named arguments', () => {
+    const camelCaseSource = fs.readFileSync('./build/camelCase.js');
+
+    assert.equal(camelCaseSource, `var _camelCase = require('lodash/camelCase');
+
+module.exports = function camelCase() {
+  return _camelCase(this);
+};
+`);
+  });
+
+  it('does not create proxies for functions without any arguments', () => {
+    const stubArraySource = fs.readFileSync('./build/stubArray.js');
+
+    assert.equal(stubArraySource, `module.exports = require('lodash/stubArray');
+`);
+  });
+
+});
+
+describe('lodash proxy', () => {
+
+  it('functions with varargs can be virtually bound', () => {
+    const concat = require('../build/concat');
+    assert.deepEqual([1,2,3,4], concat.call([1], [2], [3,4]));
+  });
+
+  it('native functions can be virtually bound', () => {
+    const isArray = require('../build/isArray');
+    assert.equal(isArray.call([]), true);
+    assert.equal(isArray.call(1), false);
+  });
+
+  it('functions with named arguments can be virtually boud', () => {
+    const camelCase = require('../build/camelCase');
+    assert.equal(camelCase.call('FOO_BAR'), 'fooBar');
+  });
+
+});
