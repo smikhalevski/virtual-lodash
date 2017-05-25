33384bbbd17eeb2cea4d28cca54225727d28fbc3
Savva Mikhalevski <smikhalevski@gmail.com>
2017-05-25 19:36:01 +0300

    Fixed .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 set -ex;
 
 VERSION=$(npm v lodash version);
@@ -13,3 +14,6 @@ cd build;
 npm config set '//registry.npmjs.org/:_authToken' "$NPM_TOKEN";
 npm publish;
 
+git config --global user.name 'smikhalevski'
+git config --global user.email 'smikhalevski@gmail.com'
+git push origin HEAD:$TRAVIS_BRANCH;
